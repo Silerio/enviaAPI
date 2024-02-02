50dfcebe23020428a48ba041760dc3982c565249
Erwin <erwin_30@live.com.mx>
2024-02-02 11:32:24 -0600

    handling responses from ENVIA API

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { join } from "node:path";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
+import dataGuide from './data.json';
 
 dotenv.config();
 
@@ -16,11 +17,43 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('addGuide', (guide: string) => {
-    socket.emit('newGuideNotification', guide);
+  socket.on('addGuide', async () => {
+    try {
+      const apiRespond = await fetch(`${process.env.API_ENVIA_BASE_URL}/ship/generate/`, {
+        method: 'POST',
+        body: JSON.stringify(dataGuide),
+        headers: { 
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.API_ENVIA_KEY}`,
+        },
+      });
+
+      const newGuideRespond = await apiRespond.json();
+
+      console.log('newGuide', newGuideRespond);
+      socket.emit('newGuideNotification', newGuideRespond);
+    } catch (error) {
+      console.log('ocurrio un error', error);
+      socket.emit('newGuideNotification', false);
+    }
   });
 });
 
+/*
+  Este web hook se había agregado para poder actualizar en tiempo real cuando 
+  se actualizaran los estatus de las guías pero la API de prueba no me respondía, 
+  solamente cuando probaba el webhook (al agregar onShipmentStatusUpdate en el dashboard) funcionaba,
+  pero al momento de actualizar el registro, no tenía respuesta de parte del API.
+
+  Por eso se descarto la funcionalidad pero me parece que no era parte del ejercicio,
+  aunque me hubiera gustado agregarlo.
+
+  app.get("/webhook", (req: Request, res: Response) => {
+    console.log('webhook has been reach');
+    console.log(req);
+  });
+*/
+
 httpServer.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Servicios ejecutandose en: http://localhost:${port}`);
 });
