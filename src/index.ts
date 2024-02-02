ece6a0dc0f68cb26d1c3085d39f6b4821932ea56
Erwin <erwin_30@live.com.mx>
2024-02-02 08:35:59 -0600

    include socket.io for web sockets functionality

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { join } from "node:path";
+import { Server } from "socket.io";
 import { createServer } from "node:http";
 
 dotenv.config();
@@ -7,6 +9,17 @@ dotenv.config();
 const app: Express = express();
 const httpServer = createServer(app);
 const port = process.env.PORT || 3030;
+const io = new Server(httpServer);
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(join(__dirname, 'frontend.html'));
+});
+
+io.on('connection', (socket) => {
+  socket.on('addGuide', (guide: string) => {
+    socket.emit('newGuideNotification', guide);
+  });
+});
 
 httpServer.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
